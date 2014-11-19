a8e3e1de52df3fec622454b04c87be263ea2f2e8
Jed <jed2161@kodos.ccnmtl.columbia.edu>
2014-11-19 16:13:34 -0500

    update js and add user profiles

---
+++ uelc/main/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UserProfile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('profile_type', models.CharField(max_length=12, choices=[(b'admin', b'Administrator'), (b'assistant', b'Assistant'), (b'group_user', b'Group User')])),
+                ('user', models.OneToOneField(related_name=b'profile', to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'ordering': ['user'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- uelc/main/models.py
+++ uelc/main/models.py
@@ -1,3 +1,32 @@
-# from django.db import models
+from django.db import models
+from django.forms import ModelForm
+from django.contrib.auth.models import User
 
-# define your models here
+
+class UserProfile(models.Model):
+    PROFILE_CHOICES = (
+        ('admin', 'Administrator'),
+        ('assistant', 'Assistant'),
+        ('group_user', 'Group User'),
+    )
+    user = models.OneToOneField(User, related_name="profile")
+    profile_type = models.CharField(max_length=12, choices=PROFILE_CHOICES)
+
+    def __unicode__(self):
+        return self.user.username
+
+    class Meta:
+        ordering = ["user"]
+
+    def display_name(self):
+        return '%s - %s' % (self.user.first_name)
+
+    def is_admin(self):
+        return self.profile_type == 'AD'
+
+    def is_assistant(self):
+        return self.profile_type == 'AS'
+
+    def is_group_user(self):
+        return self.profile_type == 'GU'
+
