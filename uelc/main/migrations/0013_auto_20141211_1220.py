f2b0c5ba303b5fef878276486151932cd1c87e6f
James Davis <jdavis1@jamess-macbook-pro.local.ccnmtl.columbia.edu>
2014-12-11 15:11:20 -0500

    add migration

---
+++ uelc/main/migrations/0013_auto_20141211_1220.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0012_auto_20141208_1107'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='casemap',
+            name='value',
+            field=models.DecimalField(max_digits=6, decimal_places=2, blank=True),
+        ),
+    ]
