34cba997b4c6936503c59ab1b645cf0cd5b29a23
Anders Pearson <anders@columbia.edu>
2015-01-26 20:28:53 +0100

    some test coverage on main.models

--- uelc/main/tests/factories.py
+++ uelc/main/tests/factories.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from pagetree.models import Hierarchy
+from uelc.main.models import (
+    Cohort, UserProfile, Case, CaseMap, TextBlockDT, UELCHandler)
 import factory
 
 
@@ -13,3 +15,37 @@ class HierarchyFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Hierarchy
     base_url = "/"
     name = "main"
+
+
+class CohortFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Cohort
+    name = factory.Sequence(lambda n: "cohort %03d" % n)
+
+
+class UserProfileFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = UserProfile
+    user = factory.SubFactory(UserFactory)
+    profile_type = "admin"
+
+
+class CaseFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = Case
+    name = factory.Sequence(lambda n: "case %03d" % n)
+    hierarchy = factory.SubFactory(HierarchyFactory)
+    cohort = factory.SubFactory(CohortFactory)
+
+
+class CaseMapFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = CaseMap
+    case = factory.SubFactory(CaseFactory)
+    user = factory.SubFactory(UserFactory)
+
+
+class TextBlockDTFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = TextBlockDT
+
+
+class UELCHandlerFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = UELCHandler
+    depth = 1
+    hierarchy = factory.SubFactory(HierarchyFactory)
--- uelc/main/tests/test_models.py
+++ uelc/main/tests/test_models.py
@@ -1,6 +1,87 @@
 from django.test import TestCase
+from .factories import (
+    CohortFactory, UserProfileFactory, CaseFactory, CaseMapFactory,
+    TextBlockDTFactory, UELCHandlerFactory)
+from uelc.main.models import TextBlockDT
 
 
-class BasicModelTest(TestCase):
-    def test_dummy(self):
-        assert True
+class CohortTest(TestCase):
+    def test_unicode(self):
+        c = CohortFactory()
+        self.assertTrue(str(c).startswith("cohort "))
+
+
+class UserProfileTest(TestCase):
+    def test_cohorts(self):
+        up = UserProfileFactory()
+        self.assertEqual(up.cohorts, "")
+
+    def test_unicode(self):
+        up = UserProfileFactory()
+        self.assertTrue(str(up).startswith("user"))
+
+
+class CaseTest(TestCase):
+    def test_unicode(self):
+        c = CaseFactory()
+        self.assertTrue(str(c).startswith("case "))
+
+
+class CaseMapTest(TestCase):
+    def test_get_value(self):
+        cm = CaseMapFactory()
+        self.assertEqual(cm.get_value(), "")
+
+    def test_add_value_places_noop(self):
+        cm = CaseMapFactory()
+        cm.add_value_places(0)
+
+
+class TextBlockDTTest(TestCase):
+    def test_add_form(self):
+        f = TextBlockDT.add_form()
+        self.assertTrue('body' in f.fields)
+
+    def test_edit_form(self):
+        t = TextBlockDTFactory()
+        self.assertTrue('body' in t.edit_form().fields)
+
+    def test_save(self):
+        t = TextBlockDTFactory()
+        vals = dict(body="a new body",
+                    after_decision="new after decision",
+                    choice="new choice")
+        t.edit(vals, None)
+        self.assertEqual(t.body, "a new body")
+        self.assertEqual(t.after_decision, "new after decision")
+        self.assertEqual(t.choice, "new choice")
+
+    def test_summary_render_short(self):
+        t = TextBlockDTFactory(body="a body <>")
+        self.assertEqual(t.summary_render(), "a body &lt;>")
+
+    def test_summary_render_long(self):
+        t = TextBlockDTFactory(body="foo" * 100)
+        self.assertTrue(t.summary_render().endswith("..."))
+
+
+class UELCHandlerTest(TestCase):
+    def test_populate_map_obj_empty(self):
+        u = UELCHandlerFactory()
+        # have to be careful to explicitly reset map_obj
+        # see PMT #99062
+        u.map_obj = {}
+        u.populate_map_obj([])
+        self.assertEqual(u.map_obj, {})
+
+    def test_populate_map_obj(self):
+        u = UELCHandlerFactory()
+        # have to be careful to explicitly reset map_obj
+        # see PMT #99062
+        u.map_obj = {}
+        u.populate_map_obj([1, 2, 3])
+        self.assertEqual(
+            u.map_obj,
+            {'p1c1': {'tree_index': 1, 'value': 2},
+             'p1pre': {'tree_index': 0, 'value': 1},
+             'p2pre': {'tree_index': 2, 'value': 3}})
