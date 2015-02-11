a9ca9782ff4ddd63c1aa81b42a82526baedf1a32
James Davis <jdavis1@jamess-macbook-pro.local.ccnmtl.columbia.edu>
2015-02-11 16:42:26 -0500

    adding template tag

---
+++ uelc/main/templatetags/part_string.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='convert')
+def convert(arg):
+    if arg > 1:
+        part = str(arg).split('.')[0]
+        choice = str(arg).split('.')[1]
+        part_choice = 'part' + part + 'choice' + choice
+        return part_choice
+    else:
+        return 'part' + str(arg)
