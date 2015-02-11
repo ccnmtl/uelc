6180275a401f9ccee1d7f82211c51d6e9164f402
James Davis <jdavis1@jamess-macbook-pro.local.ccnmtl.columbia.edu>
2015-02-11 16:34:46 -0500

    changes

--- media/js/uelc_admin/uelc_facilitator.js
+++ media/js/uelc_admin/uelc_facilitator.js
@@ -2,14 +2,44 @@ var UELCAdmin;
 UELCAdmin = {
     Admin: function() {
         this.init = function() {
+            this.set_parts_on_gateblocks();
             jQuery('.library-item-user-select').multiselect();
             jQuery('[data-toggle="tooltip"]').tooltip({
                 'placement': 'top'
             });
-
             this.deleteLibraryItem();
         };
+        this.set_parts_on_gateblocks = function() {
+            var partOneElms = jQuery('.part1:first-child');
+            var partTwoElms = [];
+            partTwoElms.push(jQuery('.part1:last').next())
+            partTwoElms.push(jQuery('.part1:first').parent()
+                .find('.part1').last().next())
+            partOneElms.each(function() {
+                html = "<div class='part1text'>Part 1 </div>";
+                jQuery(this).prepend(html)
+            })
 
+            for (i = 0; i < partTwoElms.length; i++) {
+                    var elm = partTwoElms[i];
+                    var classname = elm.attr('class')
+                    var html2;
+                    console.log(elm.hasClass('part2choice2'));
+                    switch(elm) {
+                        case elm.hasClass("part2choice1"):
+                            html2 = jQuery("<div class='part2text'>Part 2 choice 1</div>");
+                            break;
+                        case elm.hasClass("part2choice2"):
+                            html2 = jQuery("<div class='part2text'>Part 2 choice 2</div>");
+                            break;
+                        case elm.hasClass("part2choice3"):
+                            html2 = jQuery("<div class='part2text'>Part 2 choice 3</div>");
+                            break;
+                    }
+                    elm.prepend(html2)
+            }
+        
+        }
         this.deleteLibraryItem = function() {
             jQuery('.library-item-admin .glyphicon-trash').click(function() {
                 var retVal = confirm('Do you want to delete the item?');
