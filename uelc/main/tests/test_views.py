7537f104b8ada5073028f7deb5403a8acb7e9373
Cassandra Dunlop <cdulop@columbia.com>
2015-02-15 19:31:41 -0500

    update

--- uelc/main/tests/test_views.py
+++ uelc/main/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test.client import Client
 from pagetree.helpers import get_hierarchy
+from pagetree.tests.factories import get_hierarchy
 from django.contrib.auth.models import User
 
 
@@ -72,3 +73,28 @@ class PagetreeViewTestsLoggedIn(TestCase):
     def test_instructor_page(self):
         r = self.c.get("/pages/main/instructor/section-1/")
         self.assertEqual(r.status_code, 200)
+
+
+class TestGroupUserLoggedInViews(TestCase):
+    def setUp(self):
+        ModuleFactory("main", "/pages/main/")
+        self.hierarchy = Hierarchy.objects.get(name='main')
+        self.section = self.hierarchy.get_root().get_first_leaf()
+
+        self.grp_usr = GroupUpFactory().user
+        self.client = Client()
+        self.client.login(username=self.grp_usr.username, password="test")
+
+    def test_edit_page_form(self):
+        response = self.client.get(self.section.get_edit_url())
+        self.assertEqual(response.status_code, 302)
+
+    def test_page(self):
+        response = self.client.get(self.section.get_absolute_url(),
+                                   follow=True)
+        self.assertEqual(response.status_code, 200)
+ 
+    def test_index(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, 'main/index.html')
+
