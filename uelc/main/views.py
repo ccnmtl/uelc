5b79af09cbc04da5d40ec8ed7a105fd573912505
James Davis <jdavis1@jamess-macbook-pro.local.ccnmtl.columbia.edu>
2015-03-03 15:04:22 -0500

    changes to templates - adding in nav to base

--- uelc/main/views.py
+++ uelc/main/views.py
@@ -29,6 +29,13 @@ class LoggedInMixinSuperuser(object):
         return super(LoggedInMixinSuperuser, self).dispatch(*args, **kwargs)
 
 
+class LoggedInMixinFacilitator(object):
+    @method_decorator(user_passes_test(
+        lambda u: not u.profile.profile_type == "group_user"))
+    def dispatch(self, *args, **kwargs):
+        return super(LoggedInMixinFacilitator, self).dispatch(*args, **kwargs)
+
+
 class LoggedInMixin(object):
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
@@ -98,21 +105,26 @@ def reset_page(section, request):
     return HttpResponseRedirect(section.get_absolute_url())
 
 
+def get_root_context(request):
+    context = dict()
+    try:
+        cases = get_cases(request)
+        if cases:
+            roots = [(case.hierarchy.get_absolute_url(),
+                      case.hierarchy.name)
+                     for case in cases]
+            context = dict(roots=roots)
+    except ObjectDoesNotExist:
+        pass
+    return context
+
+
 class IndexView(TemplateView):
     template_name = "main/index.html"
 
     def get(self, request):
-        context = dict()
-        try:
-            cases = get_cases(request)
-            if cases:
-                roots = [(case.hierarchy.get_absolute_url(),
-                          case.hierarchy.name)
-                         for case in cases]
-                context = dict(roots=roots)
-        except ObjectDoesNotExist:
-            pass
-        return render(request, self.template_name, context)
+        root_context = get_root_context(request)
+        return render(request, self.template_name, root_context)
 
 
 def has_responses(section):
@@ -336,7 +348,7 @@ class UELCEditView(LoggedInMixinSuperuser,
     template_name = "pagetree/edit_page.html"
 
 
-class FacilitatorView(LoggedInMixinSuperuser,
+class FacilitatorView(LoggedInMixinFacilitator,
                       DynamicHierarchyMixin,
                       TemplateView,
                       SectionMixin):
@@ -435,6 +447,7 @@ class FacilitatorView(LoggedInMixinSuperuser,
         user = self.request.user
         section = self.get_section(path)
         root = section.hierarchy.get_root()
+        roots = get_root_context(self.request)
         hierarchy = section.hierarchy
         case = Case.objects.get(hierarchy=hierarchy)
         library_item = LibraryItem
@@ -478,6 +491,7 @@ class FacilitatorView(LoggedInMixinSuperuser,
                        library_item=library_item,
                        library_items=library_items,
                        case=case,
+                       roots=roots['roots']
                        )
         context.update(self.get_extra_context())
         return context
