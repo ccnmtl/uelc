c3ff786f22023a7dec0363ffec0e8fe43de0972c
coati-00 <coati.00@gmail.com>
2015-03-13 16:17:24 -0400

    flake clean up

--- uelc/main/tests/__init__.py
+++ uelc/main/tests/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 from test_models import *
 from test_views import *
+from test_helper_functions import *
--- uelc/main/tests/test_helper_functions.py
+++ uelc/main/tests/test_helper_functions.py
@@ -1,24 +1,21 @@
 from django.test import TestCase
-# from django.test.client import Client
-from pagetree.helpers import get_hierarchy
 from factories import GroupUpFactory, HierarchyFactory
 from quizblock.tests.test_models import FakeReq
-# from pagetree.tests.factories import ModuleFactory
-# from quizblock.tests.test_models import FakeReq
 from gate_block.models import GateBlock
 from uelc.main.models import CaseQuiz
 from uelc.main.helper_functions import (
-    admin_ajax_page_submit, admin_ajax_reset_page)
+    admin_ajax_page_submit, admin_ajax_reset_page,
+    page_submit, reset_page)
 
 
-class TestHelperFunctions(TestCase):
+class TestGateBlockandCaseQuizUnlockedFunctions(TestCase):
 
     def setUp(self):
         self.hierarchy = HierarchyFactory(name="main", base_url="/pages/main/")
         self.root = self.hierarchy.get_root()
         self.r = FakeReq()
         self.r.POST = {'description': 'description', 'rhetorical': False,
-                  'allow_redo': True, 'show_submit_state': False}
+                       'allow_redo': True, 'show_submit_state': False}
         self.casequiz = CaseQuiz.create(self.r)
         self.root.add_pageblock_from_dict(self.casequiz.as_dict())
         self.section = self.hierarchy.get_root().get_first_leaf()
@@ -29,31 +26,68 @@ class TestHelperFunctions(TestCase):
         self.grp_usr_profile = GroupUpFactory()
 
     def test_admin_ajax_page_submit(self):
-        '''There are two unlock methods - one in CaseQuiz and another in GateBlock do we need both?
-        Guess I'll test both....'''
-        # from nose.tools import set_trace
-        # set_trace()
-        # self.casequiz
-        # self.assertFalse(self.section.unlocked())
-        self.assertFalse(self.gate_block.unlocked(self.grp_usr_profile.user, self.section))
-        """admin_ajax_page_submit(section, user)"""
+        '''There are two unlock methods - one in
+        CaseQuiz and another in GateBlock'''
+        self.assertFalse(
+            self.gate_block.unlocked(self.grp_usr_profile.user, self.section))
+        self.assertFalse(
+            self.casequiz.unlocked(self.grp_usr_profile.user, self.section))
         admin_ajax_page_submit(self.section, self.grp_usr_profile.user)
-        '''This line keeps throwing errors the function signature for
-        GateBlock unlocked is unlocked(self, user, section): and calling
-        admin_ajax_page_submit should be creating a submission object for the user'''
-        self.assertTrue(self.gate_block.unlocked(self.grp_usr_profile.user, self.section))
+        '''To test CaseQuiz unlocked we need to create a user visit...'''
+        self.assertTrue(
+            self.gate_block.unlocked(self.grp_usr_profile.user, self.section))
 
     def test_admin_ajax_reset_page(self):
-        pass
-#         self.assertTrue(self.section.unlocked(self.grp_usr_profile.user))
-#         admin_ajax_reset_page(self.section, self.grp_usr_profile.user)
-#         self.assertFalse(self.section.unlocked(self.grp_usr_profile.user))
-
-    def test_page_submit(self):
-        pass
+        admin_ajax_page_submit(self.section, self.grp_usr_profile.user)
+        self.assertTrue(
+            self.gate_block.unlocked(self.grp_usr_profile.user, self.section))
+        admin_ajax_reset_page(self.section, self.grp_usr_profile.user)
+        self.assertFalse(self.gate_block.unlocked(
+            self.grp_usr_profile.user, self.section))
 
     def test_reset_page(self):
-        pass
+        self.otr = FakeReq()
+        self.otr.user = self.grp_usr_profile.user
+        admin_ajax_page_submit(self.section, self.grp_usr_profile.user)
+        reset_page(self.section, self.otr)
+        self.assertFalse(
+            self.gate_block.unlocked(self.grp_usr_profile.user, self.section))
+
+
+class TestPageSubmitFunctions(TestCase):
+
+    def setUp(self):
+        self.hierarchy = HierarchyFactory(name="main", base_url="/pages/main/")
+        self.root = self.hierarchy.get_root()
+        self.root.add_child_section_from_dict(
+            {'label': "One", 'slug': "one",
+             'children': [{'label': "Three", 'slug': "introduction"}]})
+        self.root.add_child_section_from_dict({'label': "Two", 'slug': "two"})
+        blocks = [{'label': 'Welcome UELC',
+                   'css_extra': '',
+                   'block_type': 'Test Block',
+                   'body': 'You should now use the edit link to add content'}]
+        self.root.add_child_section_from_dict({'label': 'Four', 'slug': 'four',
+                                               'pageblocks': blocks})
+        self.section = self.root.get_first_child()
+        self.last_section = self.root.get_last_child()
+        self.otr = FakeReq()
+        self.otr.POST = {'description': 'description', 'rhetorical': False,
+                         'allow_redo': True, 'show_submit_state': False}
+        self.grp_usr_profile = GroupUpFactory()
+        self.otr.user = self.grp_usr_profile.user
+
+    def test_page_submit(self):
+        # from nose.tools import set_trace
+        # set_trace()
+        self.first_request = page_submit(self.section, self.otr)
+        self.assertEqual(
+            self.first_request['Location'],
+            self.section.get_next().get_absolute_url())
+        self.last_request = page_submit(self.last_section, self.otr)
+        self.assertEqual(
+            self.last_request['Location'],
+            self.last_section.get_absolute_url())
 
     def test_get_root_context(self):
         pass
