af14cfd264251740ece8e9019c7cb764a06cc99f
James Davis <jdavis1@jamess-macbook-pro.local.ccnmtl.columbia.edu>
2015-03-30 11:55:16 -0400

    prod settings

--- uelc/settings_production.py
+++ uelc/settings_production.py
@@ -1,15 +1,13 @@
 # flake8: noqa
 from settings_shared import *
+import os
+import sys
 
 TEMPLATE_DIRS = (
-    "/var/www/uelc/uelc/uelc/templates",
+    os.path.join(os.path.dirname(__file__), "templates"),
 )
 
 MEDIA_ROOT = '/var/www/uelc/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/uelc/uelc/sitemedia'),
-)
 
 DATABASES = {
     'default': {
@@ -22,12 +20,31 @@ DATABASES = {
     }
 }
 
-COMPRESS_ROOT = "/var/www/uelc/uelc/media/"
+DEBUG = False
+
+AWS_STORAGE_BUCKET_NAME = "ccnmtl-uelc-static-stage"
+AWS_PRELOAD_METADATA = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_STORAGE = 'uelc.s3utils.CompressorS3BotoStorage'
+S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+COMPRESS_ROOT = STATIC_ROOT
+COMPRESS_URL = STATIC_URL
+DEFAULT_FILE_STORAGE = 'uelc.s3utils.MediaRootS3BotoStorage'
+MEDIA_URL = S3_URL + 'media/'
+COMPRESS_STORAGE = 'uelc.s3utils.CompressorS3BotoStorage'
+AWS_QUERYSTRING_AUTH = False
+
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
+STAGING_ENV = True
+
+STATSD_PREFIX = 'uelc-prod'
 
 if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django.raven_compat')				
+    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
 
 try:
     from local_settings import *
