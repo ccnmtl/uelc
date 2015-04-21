77955641fdb60c18f3c3e88e93467cc5b25984f3
coati-00 <coati.00@gmail.com>
2015-04-21 13:53:45 -0400

    update

--- media/js/uelc_admin/web_socks.js
+++ media/js/uelc_admin/web_socks.js
@@ -1,68 +1,37 @@
-var conn;
-var currentRefresh = 1000;
-var defaultRefresh = 1000;
-var maxRefresh = 1000 * 5 * 60; // 5 minutes
-
-var updateToken = function() {
-    $.ajax({
-        url: window.fresh_token_url,
-        type: "get",
-        dateType: 'json',
-        error: function(evt) {
-            setTimeout(updateToken, currentRefresh);
-            console.log("UpdateToken Error");
-        },
-        success: function(d) {
-            window.token = d.token;
-            console.log("UpdateToken Success!");
-        }
-    });
-};
-
-
-var onMessage = function (evt) {
-    var envelope = JSON.parse(evt.data);
-    var data = JSON.parse(envelope.content);
-    console.log(envelope);
-    console.log(data);
-    //console.log();
-
-//    var entry = $("<div/>");
-//    entry.addClass("row");
-//    var d = new Date();
-//    var hours = d.getHours();
-//    var minutes = d.getMinutes();
-//
-//    if (minutes < 10) {
-//        minutes = "0" + minutes;
-//    }
-//    entry.append("<div class='span1 timestamp'>" + hours + ":" + minutes + "</div>");
-//    entry.append("<div class='span2 nick'>&lt;" + data.username + "&gt;</div>");
-//    entry.append("<div class='span5 ircmessage'>" + data.message_text + "</div>");
-//    appendLog(entry);
-//    MathJax.Hub.Queue(["Typeset",MathJax.Hub, entry[0]]);
-};
-var connectSocket = function() {
-	//console.log("window.websockets_base");
-	//console.log(window.websockets_base);
-	//console.log("window.token");
-	//console.log(window.token);
-	//console.log();
-	//console.log();
-    conn = new WebSocket("wss://" + window.location.hostname + ":5555/facilitator/fresh_token/");//":8000" + window.location.pathname);
-    console.log(conn);
-	//console.log(conn);
-    //window.websockets_base + "?token=" + window.token);
-    //conn.onclose = requestFailed;
-    //conn.onmessage = onMessage;
-    conn.onopen = function (evt) {
-    	console.log(evt.data);
-        //currentRefresh = defaultRefresh;
-        //appendLog($("<div class='alert alert-info'><b>Connected to server.</b></div>"));
+$(function() {
+	var conn;
+	var currentRefresh = 1000;
+	var defaultRefresh = 1000;
+	var maxRefresh = 1000 * 5 * 60; // 5 minutes
+	
+	var updateToken = function() {
+	    $.ajax({
+	        url: window.fresh_token_url,
+	        type: "get",
+	        dateType: 'json',
+	        error: function(evt) {
+	            setTimeout(updateToken, currentRefresh);
+	            console.log("UpdateToken Error");
+	        },
+	        success: function(d) {
+	            window.token = d.token;
+	            console.log("UpdateToken Success!");
+	        }
+	    });
+	};
+	
+	
+	var connectSocket = function() {
+        conn = new WebSocket(window.websockets_base + "?token=" + window.token);
+        //conn.onclose = requestFailed;
+        //conn.onmessage = onMessage;
+        conn.onopen = function (evt) {
+            currentRefresh = defaultRefresh;
+            alert("connectSocket() connected!");
+            //appendLog($("<div class='alert alert-info'><b>Connected to server.</b></div>"));
+        };
     };
-};
 
-jQuery(document).ready(function() {
 
 	console.log("window.websockets_base");
 	console.log(window.websockets_base);
