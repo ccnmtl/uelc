f87707c45a4ab1103a3e3bd10563e9a2544cde92
coati-00 <coati.00@gmail.com>
2015-04-22 14:18:23 -0400

    changing gen_token and fresh_token

--- uelc/main/helper_functions.py
+++ uelc/main/helper_functions.py
@@ -9,8 +9,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import get_object_or_404
 
 from pagetree.generic.views import generic_instructor_page, generic_edit_page
+from pagetree.models import UserPageVisit, Hierarchy
 
 from gate_block.models import GateSubmission
 from uelc.main.models import CaseMap, Case
@@ -137,34 +139,28 @@ def visit_root(section, fallback_url):
 
 
 @login_required
-def fresh_token(request):
-    return HttpResponse(
-        json.dumps(dict(token=gen_token(request))),
-        content_type="application/json")
-#return dict(room=room, token=gen_token(request, room.id),
-#                websockets_base=settings.WINDSOCK_WEBSOCKETS_BASE)
-def gen_token(request):
+def fresh_token(request, hierarchy_name):
+    hierarchy = get_object_or_404(Hierarchy, name=hierarchy_name)
+    return dict(hierarchy=hierarchy, token=gen_token(request, hierarchy.name),
+                websockets_base=settings.WINDSOCK_WEBSOCKETS_BASE)
+
+
+def gen_token(request, hierarchy_name):
     print "Inside gen_token"
     username = request.user.username
-    user_id = request.user.pk
-    print "user_id"
-    print user_id
-    sub_prefix = "%s.profile_%d" % (settings.ZMQ_APPNAME, user_id)
+    sub_prefix = "%s.pages/%s/facilitator/" % (settings.ZMQ_APPNAME, hierarchy_name)
     pub_prefix = sub_prefix + "." + username
     now = int(time.mktime(datetime.now().timetuple()))
     salt = randint(0, 2 ** 20)
     ip_address = (request.META.get("HTTP_X_FORWARDED_FOR", "")
                   or request.META.get("REMOTE_ADDR", ""))
-
+    print "After IP"
     hmc = hmac.new(settings.WINDSOCK_SECRET,
                    '%s:%s:%s:%d:%d:%s' % (username, sub_prefix,
                                           pub_prefix, now, salt,
                                           ip_address),
                    hashlib.sha1
                    ).hexdigest()
-    print '%s:%s:%s:%d:%d:%s:%s' % (username, sub_prefix,
-                                     pub_prefix, now, salt,
-                                     ip_address, hmc)
     return '%s:%s:%s:%d:%d:%s:%s' % (username, sub_prefix,
                                      pub_prefix, now, salt,
                                      ip_address, hmc)
