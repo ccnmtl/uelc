c2cb6a07c3912881a4e9073e1cbe2d508aa7f47f
coati-00 <coati.00@gmail.com>
2015-04-24 18:11:43 -0400

    update

--- uelc/main/helper_functions.py
+++ uelc/main/helper_functions.py
@@ -171,8 +171,12 @@ def fresh_grp_token(request, section_id):
 
 
 def gen_group_token(request, section_pk):
+    print "gen_group_token"
+    print gen_group_token
     username = request.user.username
     sub_prefix = "%s.%d" % (settings.ZMQ_APPNAME, section_pk)
+    print "sub_prefix"
+    print sub_prefix
     pub_prefix = sub_prefix + "." + username
     now = int(time.mktime(datetime.now().timetuple()))
     salt = randint(0, 2 ** 20)
