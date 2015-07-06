ebb69aa4afa7678d56f09f8a8ea8bc6d03dfc987
James Davis <jedavis@columbia.edu>
2015-07-06 11:31:00 -0400

    explicitly set ckeditor options

--- media/ckeditor/ckeditor/config.js
+++ media/ckeditor/ckeditor/config.js
@@ -4,7 +4,20 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.toolbar = 'MyToolbar';
+ 
+    config.toolbar_MyToolbar =
+    [
+        { name: 'document', items : [ 'Source' ] },
+        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+        { name: 'editing', items : [ 'Find','-','Scayt' ] },
+        { name: 'insert', items : [ 'Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'
+                 ,'Iframe' ] },
+                '/',
+        { name: 'styles', items : [ 'Styles','Format' ] },
+        { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+        { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+        { name: 'tools', items : [ 'Maximize','-','About' ] },
+    ];
 };
