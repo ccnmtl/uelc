1c8bd9c1b391e53524ce11cdff4cb36189f8297b
coati-00 <coati.00@gmail.com>
2015-09-04 17:24:14 -0400

    added some tests

--- curveball/tests/test_models.py
+++ curveball/tests/test_models.py
@@ -26,6 +26,19 @@ class CurveballBlockTest(TestCase):
     def test_is_json_serializable(self):
         json.dumps(self.c.as_dict())
 
+    def test_redirect_on_submit(self):
+        self.assertFalse(self.c.redirect_to_self_on_submit())
+
+    def test_needs_submit(self):
+        self.assertTrue(self.c.needs_submit())
+
+    def test_allow_redo(self):
+        self.assertFalse(self.c.allow_redo())
+
+    def test_edit_form(self):
+        f = self.c.edit_form()
+        self.assertEqual(None, f.full_clean())
+
 
 class CurveballSubmissionTest(TestCase):
     def setUp(self):
