a1d5eb4c8f48bc00e3a501e56aef25deddad6bb7
Nik Nyby <nnyby@columbia.edu>
2016-09-08 12:33:27 -0400

    Add caching to a few UELCHandler methods - PMT #107288
    This reduces the number of queries for this page by about 500 in my test
    case.

---
+++ uelc/main/utils.py
@@ -0,0 +1,6 @@
+from django.core.cache import cache
+
+
+def clear_handler_cache(node):
+    cache.delete('uelc.{}.get_part_by_section'.format(node.pk))
+    cache.delete('uelc.{}.is_next_curveball'.format(node.pk))
