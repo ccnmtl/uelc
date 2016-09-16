108a3639c6c22349cb684f166aa57e00d5067291
Nik Nyby <nnyby@columbia.edu>
2016-09-16 11:04:33 -0400

    Add caching for FacilitatorView.get_tree_depth - PMT #107228

--- uelc/main/utils.py
+++ uelc/main/utils.py
@@ -4,3 +4,4 @@ from django.core.cache import cache
 def clear_handler_cache(node):
     cache.delete('uelc.{}.get_part_by_section'.format(node.pk))
     cache.delete('uelc.{}.is_next_curveball'.format(node.pk))
+    cache.delete('uelc.{}.get_tree_depth'.format(node.pk))
