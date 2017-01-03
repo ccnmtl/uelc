60828add892593a4342d7a7901d3028ba7d59725
Susan Dreher <sdreher@columbia.edu>
2017-01-03 07:49:25 -0500

    remove unused code

--- uelc/mixins.py
+++ uelc/mixins.py
@@ -1,8 +1,6 @@
-import json
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.http import HttpResponse
 from django.http.response import (
-    HttpResponseForbidden, HttpResponseNotFound, HttpResponseNotAllowed
+    HttpResponseForbidden, HttpResponseNotFound
 )
 from django.utils.decorators import method_decorator
 from pagetree.helpers import get_section_from_path
@@ -74,34 +72,3 @@ class RestrictedModuleMixin(object):
                                  self).dispatch(*args, **kwargs)
         return HttpResponseForbidden("you don't have permission")
 
-
-def ajax_required(func):
-    """
-    AJAX request required decorator
-    use it in your views:
-    @ajax_required
-    def my_view(request):
-    """
-
-    def wrap(request, *args, **kwargs):
-        if not request.is_ajax():
-            return HttpResponseNotAllowed("")
-        return func(request, *args, **kwargs)
-
-    wrap.__doc__ = func.__doc__
-    wrap.__name__ = func.__name__
-    return wrap
-
-
-class JSONResponseMixin(object):
-    @method_decorator(ajax_required)
-    def dispatch(self, *args, **kwargs):
-        return super(JSONResponseMixin, self).dispatch(*args, **kwargs)
-
-    def render_to_json_response(self, context, **response_kwargs):
-        """
-        Returns a JSON response, transforming 'context' to make the payload.
-        """
-        return HttpResponse(json.dumps(context),
-                            content_type='application/json',
-                            **response_kwargs)
